2f157eac457940ee70ad7df8a2ab641c994bfd46
brightcaiusamegbor <brightcaiusamegbor@gmail.com>
2021-09-25 23:35:14 +0000

    added responsiveness to register-driver form

--- src/pages/register_driver/index.js
+++ src/pages/register_driver/index.js
@@ -31,75 +31,90 @@ const RegisterDriver = () => {
     return(
         <Fragment>
             <div className="container d-flex flex-column align-items-center justify-content-center">
+                <div className="rd-rgp">
+                    <a href="/ride">Ride</a>
+                </div>
                 <Row className="">
-                    <Col md={8} sm={10} lg={12}>
+                    <Col md={10} sm={10} lg={11}>
                     
-                    <Card className="p-4 register-driver-form mt-3">
+                        <Card className="p-4 register-driver-form mt-3">
 
-                    <h3 className="mb-3">Open Ride | Register as a driver</h3>
-                    <Form className="">
-                    <Form.Group className="mb-3">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email address" />
-                    </Form.Group>
+                            <h3 className="mb-3">Open Ride | Register as a driver</h3>
+                            <Form className="">
+                            <Form.Group className="mb-3">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email address" />
+                            </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Phone number</Form.Label>
-                            <InputGroup>
-                            <span className="country-prefix">+233</span>
-                                <Form.Control className="phone-field" type="phone" placeholder="Enter phone number" />
+                            <Form.Group className="mb-3">
+                                <Form.Label>Phone number</Form.Label>
+                                    <InputGroup>
+                                    <span className="country-prefix">+233</span>
+                                        <Form.Control className="phone-field" type="phone" placeholder="Enter phone number" />
 
-                                {/* <FieldControl
-                                    render={TextInput}
-                                    meta={{ label: "Full name", name: "Full name" }}
-                                    id="name"
-                                    name="name" /> */}
-                            </InputGroup>
-                    </Form.Group>
+                                        {/* <FieldControl
+                                            render={TextInput}
+                                            meta={{ label: "Full name", name: "Full name" }}
+                                            id="name"
+                                            name="name" /> */}
+                                    </InputGroup>
+                            </Form.Group>
 
-                        <Form.Group className="mb-3">
-                            <Form.Label>City</Form.Label>
-                            <select className="form-control" aria-label="Default select example">
-                                <option disabled>Select city</option>
-                                <option value="1">Accra</option>
-                                <option value="2">Kumasi</option>
-                                <option value="3">Cape Coast</option>
-                            </select>
-                        </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>City</Form.Label>
+                                    <select className="form-control" aria-label="Default select example">
+                                        <option disabled>Select city</option>
+                                        <option value="1">Accra</option>
+                                        <option value="2">Kumasi</option>
+                                        <option value="3">Cape Coast</option>
+                                    </select>
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Password</Form.Label>
-                        <InputGroup>
-                                <Form.Control className="password-field" type={obsecureText ? "password" : "text"} placeholder="Enter password" />
-                                <span onClick={() => setObsecureText(!obsecureText)} className="password-visibility">
-                                    {obsecureText === true ? <MdRemoveRedEye /> : <FaEyeSlash /> } </span>
-                        </InputGroup>
-                    </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Password</Form.Label>
+                                <InputGroup>
+                                        <Form.Control className="password-field" type={obsecureText ? "password" : "text"} placeholder="Enter password" />
+                                        <span onClick={() => setObsecureText(!obsecureText)} className="password-visibility">
+                                            {obsecureText === true ? <MdRemoveRedEye /> : <FaEyeSlash /> } </span>
+                                </InputGroup>
+                            </Form.Group>
 
-                        <div className="d-flex">
-                            <div>
-                                <Button variant="secondary" type="submit">
-                                    Next
-                                </Button>
-                            </div>
+                                <div className="d-flex">
+                                    <div>
+                                        <Button variant="secondary" type="submit">
+                                            Next
+                                        </Button>
+                                    </div>
 
-                            <div className="d-flex align-items-center already-account-driver">
-                                <p className="ms-4">Already have an account? <a href="/#">Sign in</a></p>
-                            </div>
-                        </div>
+                                    <div className="d-flex align-items-center already-account-driver">
+                                        <p className="ms-3 ms-lg-4 ms-md-4 ms-xl-4 ms-xxl-4">Already have an account? <a href="/#">Sign in</a></p>
+                                    </div>
+                                </div>
 
-                        <Form.Group className="mt-3">
-                            <Form.Text className="text-muted register-driver-privacy">
-                                By signing up, I accept Open Ride Terms of Service and Privacy Policy.
-                            </Form.Text>
-                        </Form.Group>
+                                <Form.Group className="mt-3">
+                                    <Form.Text className="text-muted register-driver-privacy">
+                                        By signing up, I accept Open Ride Terms of Service and Privacy Policy.
+                                    </Form.Text>
+                                </Form.Group>
 
-                </Form>
-                
-                </Card>
-                
+                        </Form>
+                    
+                    </Card>
+
+                    
                     </Col>
                 </Row>
+                    <div className="d-flex justify-content-center mt-2 register-driver-footer">
+                        <p className="pe-3">Accessibility</p>
+                        <p className="pe-3">Privacy</p>
+                        <p className="pe-3">Terms</p>
+                    </div>
+
+                    <div className="d-flex justify-content-center register-driver-footer">
+                        <p className="text-center">&copy; {new Date().getFullYear()} Open Ride Inc. All rights reserved, Open Ride is 
+                            unregistered trademark of Bright Amegbor. 
+                        </p>
+                    </div>
             </div>
         </Fragment>
     )
